709ab12ac9dc4d78f391d2cb48adf3a4d0ca6fb3
rekameszaros <rikkemeszaros@gmail.com>
2022-11-06 22:21:07 +0100

    Navigation - added burger menu + css

--- frontend/src/pages/signup.jsx
+++ frontend/src/pages/signup.jsx
@@ -2,10 +2,12 @@ import SignUpForm from "../components/signup-form";
 import Navigation from "../components/navigation";
 function SignUp() {
   return (
-    <>
-      <Navigation />
-      <SignUpForm />
-    </>
+    <div className="SignUp" id="outer-container">
+      <Navigation pageWrapID={'page-wrap'} outerContainerId={'outer-container'}/>
+      <div id="page-wrap">
+        <SignUpForm />
+      </div>
+    </div>
   );
 }
 
