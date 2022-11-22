784635397b1a03e36de8b53c1eda4e5e321aae40
Adelina Radulescu <adel0896@stud.kea.dk>
2022-11-22 14:22:49 +0100

    tryyy

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -16,7 +16,7 @@ export class AppController {
     private userService: UserService,
     private jwtService: JwtService,
   ) {}
-  // comment to be deleted
+  // comment to be deletedddd
   // @Get()
   // getHello(): string {
   //   return this.appService.getHello();
