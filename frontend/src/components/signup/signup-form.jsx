70a0f436a31934a316803fb845fa04c238452ce7
Adelina Radulescu <adel0896@stud.kea.dk>
2022-12-26 18:31:41 +0100

    fixed spaceing in signup

--- frontend/src/components/signup/signup-form.jsx
+++ frontend/src/components/signup/signup-form.jsx
@@ -28,7 +28,7 @@ function SignUpForm() {
       placeholder: "Username",
       errorMessage: "Username should be 3-16 characters and shouldn't include any special character!",
       label: "Username",
-      pattern: "^[A-Za-z0-9]{3,16}$",
+      pattern: "^[A-Za-z0-9 ]{3,16}$",
       required: true,
     },
     {
